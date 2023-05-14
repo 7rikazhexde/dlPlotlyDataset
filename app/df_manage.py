d4c5b44f0ed86cb5a25c21f3c089e912a6e5a1ba
7rikazhexde <33836132+7rikazhexde@users.noreply.github.com>
2023-05-14 15:22:34 +0900

    feat: Added csv file download function and updated package
    - Added dcc.Download component and callback process for downloading CSV files.
    - Updated package and pyproject.toml(version 0.1.0 -> 0.2.0).
    - Added static analysis tool for development.

--- app/df_manage.py
+++ app/df_manage.py
@@ -1,16 +1,26 @@
+from typing import List
 import pandas as pd
+
 
 class DfManage:
     def __init__(self):
-        self.__data = []
-        self.__df_data = pd.DataFrame(self.__data)
+        data_list: List[List[int | float | str | None]] = []
+        """
+        data_list = [] # type: List[List[int | float | str | None]]
+        data_list = [] # type: List[Union[int,float,str,None]]
+        data_list: List[Union[int, float, str, None]] = []
+        """
+        columns_list: List[str] = []
+        self.__df_data = pd.DataFrame(data=data_list, columns=columns_list)
+        # print(self.__df_data)
+        # print(f'values: {self.__df_data.values}')
         self.__df_page_num = 0
 
     # Accessor for processing DataFrame objects obtained with the Subscan API
     def get_df(self):
         return self.__df_data
-     
-    def set_df(self,df_data):
+
+    def set_df(self, df_data):
         self.__df_data = df_data
 
     df_data = property(get_df, set_df)
@@ -18,8 +28,8 @@ class DfManage:
     # Accessor that handles page_current properties of dash_table.DataTable
     def get_df_page_num(self):
         return self.__df_page_num
-     
-    def set_df_page_num(self,df_page_num):
+
+    def set_df_page_num(self, df_page_num):
         self.__df_page_num = df_page_num
 
     df_page_num = property(get_df_page_num, set_df_page_num)
